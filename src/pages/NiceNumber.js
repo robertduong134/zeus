f5d1be84cf7d98cbb9f6aa41c0892081106cab5b
duongns <ayemedcb@gmail.com>
2021-05-24 16:53:18 +0700

    căn chỉnh css

--- src/pages/NiceNumber.js
+++ src/pages/NiceNumber.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Steps } from 'primereact/steps';
-import { Toast } from 'primereact/toast';
 import { InputText } from 'primereact/inputtext';
 import 'primeflex/primeflex.css';
+import { Button } from 'primereact/button';
 
 export default class NiceNumber extends Component {
 
@@ -27,24 +27,38 @@ export default class NiceNumber extends Component {
 
     render() {
         return (
-            <div className="wrapper">
-                <Toast ref={(el) => { this.toast = el }}></Toast>
-                <div className="p-grid">
-                    <div className="p-col-12 p-md-3">
-                        1
+            <div className="container">
+                <div className="box header">
+                    <h1>Đăng ký tài khoản số đẹp VETC</h1>
+                    <br></br>
+                </div>
+
+                <div className="box body">
+                    <div className="steps-header">
+                        <Steps model={this.items} />
                     </div>
-                    <div className="p-col-12 p-md-9">
-                        <div className="card">
-                            <h5>Đăng ký tài khoản số đẹp VETC</h5>
-                            <Steps model={this.items} />
-                            <label htmlFor="accountNo">Số tài khoản</label>
-                            <InputText id="accountNo"></InputText>
+                    <div className="steps-content">
+                        <div className="step-1">
+                            <div className="p-field p-grid">
+                                <label htmlFor="firstname3" className="p-col-fixed" style={{width:'100px'}}>Số tài khoản</label>
+                                <div className="p-col">
+                                    <InputText id="firstname3" type="text"/>
+                                </div>
+                            </div>
+                            <div className="p-field p-grid">
+                                <label htmlFor="lastname3" className="p-col-fixed" style={{width:'100px'}}>Mã giới thiệu</label>
+                                <div className="p-col">
+                                    <InputText id="lastname3" type="text"/>
+                                </div>
+                            </div>
+                            <div className="p-field">
+                                <Button label="Tiếp theo"></Button>
+                            </div>
                         </div>
                     </div>
                 </div>
+                
             </div>
-
-
         );
     }
 }
